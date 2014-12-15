f2835ac6872e0a8d536fe53be2d9a361f6e1870d
Shevek <shevek@anarres.org>
2014-12-15 14:13:52 -0800

    Unignore tests.

--- udev-jna/src/test/java/org/anarres/udev/UdevTest.java
+++ udev-jna/src/test/java/org/anarres/udev/UdevTest.java
@@ -6,7 +6,6 @@ package org.anarres.udev;
 
 import java.util.Set;
 import java.util.TreeSet;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +19,6 @@ public class UdevTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(UdevTest.class);
 
-    @Ignore
     @Test
     public void testEnumeration() {
         Udev udev = new Udev();
